af2776ad15158c485b0622316c580b7ce0b05e30
vishalsingh444 <vishalsingh444888@gmail.com>
2023-12-26 13:57:20 +0530

    completed jwt assignments

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -19,7 +19,29 @@ const app = express();
 
 
 app.get("/files",function(req,res){
-  
+  fs.readdir(path.join(__dirname,'./files/'),(err,files)=>{
+    if(err){
+      return res.status(500).json({err:'Failed to retrieve files'})
+    }
+    return res.json(files);
+  });
 })
+
+app.get("/file/:filename",function(req,res){
+  const filepath = path.join(__dirname,'./files/',req.params.filename);
+
+  fs.readFile(filepath,"utf-8",function(err,data){
+    if(err){
+      return res.status(404).send("File not found");
+    }
+    res.send(data);
+  });
+});
+
+app.all('*',function(req,res){
+  res.status(404).send("Route not found");
+});
+
+// app.listen(3000);
 
 module.exports = app;
--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -41,9 +41,155 @@
  */
   const express = require('express');
   const bodyParser = require('body-parser');
-  
+  const fs = require("fs");
   const app = express();
   
   app.use(bodyParser.json());
+
+
+  // let todos = [];
+
+  // app.get("/todos",function(req,res){
+  //   res.send(todos);
+  // })
+
+  // app.get("/todos/:id",function(req,res){
+  //   const todo = todos.find(function(element){
+  //     return element === parseInt(req.params.id);
+  //   })
+  //   if(todo){
+  //     res.json(todo);
+  //   }else{
+  //     res.status(404)
+  //   }
+  // });
+
+  // app.post("/todos",function(req,res){
+  //   const newTodo = {
+  //     id: Math.floor(Math.random() * 1000000),
+  //     title: req.body.title,
+  //     description: req.body.description
+  //   };
+  //   todos.push(newTodo);
+  //   res.status(201).json(newTodo);
+  // });
+
+  // app.put("/todos/:id",function(req,res){
+  //   const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+  //   if(todoIndex === -1){
+  //     res.status(404).send();
+  //   }else{
+  //     todos[todoIndex].title = req.body.title;
+  //     todos[todoIndex].description = req.body.description;
+  //     res.json(todos[todoIndex]);
+  //   }
+  // });
+
+
+  // app.delete("/todos/:id",function(req,res){
+  //   const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+  //   if(todoIndex === -1){
+  //     res.status(404).send();
+  //   }else{
+  //     todos.splice(todoIndex,1);
+  //     res.status(200).send();
+  //   }
+  // });
+  function findIndex(arr,id){
+    for(let i=0;i<arr.length;i++){
+      if(arr[i].id === id) return i;
+    }
+    return -1;
+  }
+
+  function removeAtIndex(arr,index){
+    let newArray = [];
+    for(let i=0;i<arr.length;i++){
+      if(i !== index) newArray.push(arr[i]);
+    }
+    return newArray;
+  }
+
+  app.get("/todos",(req,res)=>{
+    fs.readFile("todos.json","utf8",(err,data)=>{
+      if(err) throw err;  
+      res.json(JSON.parse(data))
+    });
+  });
+
+  app.get("/todos/:id",(req,res)=>{
+    fs.readFile("todos.json","utf8",(err,data)=>{
+      if(err) throw err;
+      const todos = JSON.parse(data);
+      const todoIndex = findIndex(todos,parseInt(req.params.id));
+      if(todoIndex===-1){
+        res.status(404).send();
+      }else{
+        res.json(todos[todoIndex]);
+      }
+    });
+  });
+
+
+  app.post("/todos",(req,res)=>{
+    const newTodo = { 
+      id: Math.floor(Math.random() * 1000000),
+      title: req.body.title,
+      description: req.body.description
+    };
+    fs.readFile("todos.json","utf8",(err,data)=>{
+      if(err) throw err;
+      const todos = JSON.parse(data);
+      todos.push(newTodo);
+      fs.writeFile("todos.json",JSON.stringify(todos),(err)=>{
+        if(err) throw err;
+        res.status(201).json(newTodo)
+      });
+    });
+  });
+
+  app.put("/todos/:id",(req,res)=>{
+    fs.readFile("todos.json","utf8",(err,data)=>{
+      if(err) throw err;
+      const todos = JSON.parse(data);
+      const todoIndex = findIndex(todos,parseInt(req.params.id));
+      if(todoIndex === -1){
+        res.status(404).send();
+      }else{
+        const updatadTodo = {
+          id: todos[todoIndex].id,
+          title: req.body.title,
+          description: req.body.description
+        };
+        todos[todoIndex] = updatadTodo; 
+        fs.writeFile("todos.json",JSON.stringify(todos),(err)=>{
+          if(err) throw err;
+          res.status(200).json(updatadTodo);
+        });
+      }
+    });
+  });
+
+
+  app.delete("/todos/:id",(req,res)=>{
+    fs.readFile("todos.json","utf8",(err,data)=>{
+      if(err) throw err;
+      let todos = JSON.parse(data);
+      const todoIndex = findIndex(todos,parseInt(req.params.id));
+      if(todoIndex === -1){
+        res.status(404).send();
+      }else{
+        todos = removeAtIndex(todos,todoIndex);
+        fs.writeFile("todos.json",JSON.stringify(todos),(err)=>{
+          if(err) throw err;
+          res.status(200).send();
+        });
+      }
+    });
+  });
+
+  app.use(function(req,res,next){
+    res.status(404).send();
+  });
   
   module.exports = app;
