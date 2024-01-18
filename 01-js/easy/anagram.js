22c7613cb1017214a65a7b9876fe10d58f4c21a8
vishalsingh444 <vishalsingh444888@gmail.com>
2024-01-18 10:04:05 +0530

    completed week-6 assignments

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,4 +1,4 @@
-/*
+  /*
   Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -8,11 +8,17 @@ export const Assignment3 = () => {
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
+        {name: 'lays',value: 50}
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(() => {
+        return items.reduce((accumulator,currentValue) => {
+            return accumulator + currentValue.value
+        },0)
+        
+    },[items])
     // Your code ends here
     return (
         <div>
